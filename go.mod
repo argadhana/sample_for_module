93b315370122bdf872b36c5a248bf71e2adbe7b2
argadhana <yoshspecta333@gmail.com>
2020-10-31 02:24:01 +0700

    sample for module

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/argadhana/sample_for_module
+
+go 1.15
+
+require github.com/argadhana/learn_go_module/v3 v3.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/argadhana/learn_go_module/v3"
+)
+
+func main() {
+	fmt.Println(learn_go_module.SayHello(" arga"))
+}
